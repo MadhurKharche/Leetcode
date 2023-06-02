d7c62070ead3a4b3e8913f1bea5166e2e597b636
MadhurKharche <78757405+MadhurKharche@users.noreply.github.com>
2023-06-02 22:14:55 +0530

    Added solution - LeetHub

--- Search in a Rotated Array - GFG/search-in-a-rotated-array.cpp
+++ Search in a Rotated Array - GFG/search-in-a-rotated-array.cpp
@@ -10,9 +10,25 @@ class Solution{
         // h: The ending index, you have to search the key in this range
         
         //complete the function here
-        for(int i=l; i<=h; i++){
-            if(arr[i] == key)
-                return i;
+        while(l <= h){
+            int mid = (l+h)/2;
+            
+            if(arr[mid] == key)
+                return mid;
+                
+            if(arr[l] <= arr[mid]){
+                if(arr[l]<=key && arr[mid]>=key)
+                    h = mid-1;
+                else
+                    l = mid + 1;
+            }
+            else{
+                if(arr[mid]<=key && arr[h]>=key)
+                    l = mid+1;
+                else
+                    h = mid-1;
+                    
+            }
         }
         
         return -1;
